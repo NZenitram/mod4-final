12fd2c63d47f3e3ce1a33f0e40796e42661ac1c5
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-27 18:17:10 -0700

    building out tests

--- spec/features/create_link_spec.rb
+++ spec/features/create_link_spec.rb
@@ -2,16 +2,15 @@ require "rails_helper"
 
 RSpec.describe "can create links", :js => :true do
   scenario "Create a new link" do
+    
     user = User.create!(email: "test@test.com", password_digest: BCrypt::Password.create("test"))
+    link = Link.create!(title: "Title", url: "http://www.google.com", read: true, user_id: user.id)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit "/links"
-    fill_in "Title:", :with => "Turing"
-    fill_in "URL:", :with => "http://turing.io"
-    click_on "Add Link"
 
-    expect(page).to have_text("Turing")
-    expect(page).to have_text("http://turing.io")
+    expect(page).to have_text(link.title)
+    expect(page).to have_text(link.url)
   end
 end
--- spec/features/links/user_can_view_and_submit_links_spec.rb
+++ spec/features/links/user_can_view_and_submit_links_spec.rb
@@ -3,21 +3,15 @@ require 'rails_helper'
 describe 'when an authenticated user visits the links index', :js => :true do
   context 'the user can see submitted links' do
     it 'should see a form to submit a link' do
-      user = User.create!(email: "test@test.com", password_digest: BCrypt::Password.create("test"))
-      link = Link.create!(title: "Example", url: "http://www.example.com", read: false)
-      visit '/'
-      fill_in "email", with: user.email
-      fill_in "password", with: "test"
 
-      click_button "Sign In"
+      user = User.create!(email: "test@test.com", password_digest: BCrypt::Password.create("test"))
+      link = Link.create!(title: "Example", url: "http://www.example.com", read: false, user_id: user.id)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/links'
 
       expect(page).to have_content("Hot Reads!")
-
-      fill_in "URL", with: link.url
-      fill_in "Title", with: link.title
-
-      click_button "Add Link"
-
       expect(page).to have_content(link.title)
     end
   end
