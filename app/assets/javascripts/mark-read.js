ba58d3ff06b6f6fa37b9a454a111c348b7b4b2af
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-27 18:08:00 -0700

    strike through works

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -1,33 +1,28 @@
 var $newLinkTitle, $newLinkUrl;
 
 $(document).ready(function(){
-
+  strikeThrough();
   $('.hot-read').on('click', '.mark-read', function(){
     var $this = $(this);
     var linkId = this.id;
-    
-    if (this.parentElement.children[4].innerText.trim() == "true"){
-      this.parentElement.children[4].innerText = "false"
+
+    if (this.parentElement.children[3].innerText.trim() == "true"){
+      this.parentElement.children[3].innerText = "false"
+      $(this).parent().children().first().css("text-decoration", "none")
       $.ajax({
         url: 'api/v1/links/' + linkId,
         method: 'PATCH',
         data: {read: false}
       });
     } else {
-        this.parentElement.children[4].innerText = "true"
+        this.parentElement.children[3].innerText = "true"
+        $(this).parent().children().first().css("text-decoration", "line-through")
         $.ajax({
           url: 'api/v1/links/' + linkId,
           method: 'PATCH',
           data: {read: true}
         });
       }
-
-    $.ajax({
-      url: 'api/v1/links/' + linkId,
-      method: 'PATCH',
-      data: {read: true}
-    });
-    location.reload();
   })
 
   $("#my-input").on('keyup', function() {
@@ -44,4 +39,14 @@ $(document).ready(function(){
       }
     }
    });
+
+  function strikeThrough(){
+    var li = $('.hot-read')
+
+    for (var i = 0; i < li.length; i++) {
+      if (li[i].children[3].innerText.trim() == "true") {
+        $(li[i]).parent().children().first().css("text-decoration", "line-through")
+      }
+    }
+  }
 })
